45c7e4c43a043cab83e043d52d61d632ebee8739
Branislav Lazic <brano2411@hotmail.com>
2024-03-06 12:54:10 +0100

    Refactor Ior and NEL

--- lib/ior.ml
+++ lib/ior.ml
@@ -4,14 +4,12 @@ let right a = Right a
 let left a = Left a
 let both a b = Both (a, b)
 
-let fold left_fn right_fn both_fn ior =
-  match ior with
+let fold left_fn right_fn both_fn = function
   | Left a -> left_fn a
   | Right a -> right_fn a
   | Both (a, b) -> both_fn a b
 
-let map fn ior =
-  match ior with
+let map fn = function
   | Left a -> Left a
   | Right a -> Right (fn a)
   | Both (a, b) -> Both (a, fn b)
@@ -23,8 +21,7 @@ let bimap left_fn right_fn ior =
     (fun a b -> Both (left_fn a, right_fn b))
     ior
 
-let left_map fn ior =
-  match ior with
+let left_map fn = function
   | Left a -> Left (fn a)
   | Right a -> Right a
   | Both (a, b) -> Both (fn a, b)
@@ -32,15 +29,21 @@ let left_map fn ior =
 let swap ior =
   fold (fun a -> Right a) (fun a -> Left a) (fun a b -> Both (b, a)) ior
 
-let to_option ior =
-  match ior with Left _ -> None | Right a -> Some a | Both (_, a) -> Some a
+let to_option = function
+  | Left _ -> None
+  | Right a -> Some a
+  | Both (_, a) -> Some a
 
-let to_result ior =
-  match ior with Right a -> Ok a | Left a -> Error a | Both (_, b) -> Ok b
+let to_result = function
+  | Right a -> Ok a
+  | Left a -> Error a
+  | Both (_, b) -> Ok b
 
-let get_or_else alternate ior =
-  match ior with Right a -> a | Left _ -> alternate | Both (_, a) -> a
+let get_or_else alternate = function
+  | Right a -> a
+  | Left _ -> alternate
+  | Both (_, a) -> a
 
-let is_left ior = match ior with Left _ -> true | _ -> false
-let is_right ior = match ior with Right _ -> true | _ -> false
-let is_both ior = match ior with Both _ -> true | _ -> false
+let is_left = function Left _ -> true | _ -> false
+let is_right = function Right _ -> true | _ -> false
+let is_both = function Both _ -> true | _ -> false
--- lib/non_empty_list.ml
+++ lib/non_empty_list.ml
@@ -1,33 +1,24 @@
 type 'a t = 'a * 'a list
 
 let one a = (a, [])
+let from_list = function head :: tail -> Some (head, tail) | [] -> None
+let map fn = function head, rest -> (fn head, List.map fn rest)
 
-let from_list list =
-  match list with head :: tail -> Some (head, tail) | [] -> None
-
-let map fn nel = match nel with head, rest -> (fn head, List.map fn rest)
-
-let filter fn nel =
-  match nel with
+let filter fn = function
   | head, rest ->
       from_list ((if fn head then [ head ] else []) @ List.filter fn rest)
 
-let find fn nel =
-  match nel with
+let find fn = function
   | head, rest -> if fn head then Some head else List.find_opt fn rest
 
-let exists fn nel =
-  match nel with head, rest -> fn head || List.exists fn rest
-
-let for_all fn nel =
-  match nel with head, rest -> fn head && List.for_all fn rest
+let exists fn = function head, rest -> fn head || List.exists fn rest
+let for_all fn = function head, rest -> fn head && List.for_all fn rest
 
 let zip l_nel r_nel =
   match (l_nel, r_nel) with
   | (l, rest_l), (r, rest_r) -> ((l, r), List.combine rest_l rest_r)
 
-let reverse nel =
-  match nel with
+let reverse = function
   | head, rest ->
       if List.length rest > 0 then
         let revRest = List.rev rest in
@@ -39,8 +30,8 @@ let ( <+> ) l_nel r_nel =
   | (l, rest_l), (r, rest_r) -> (l, rest_l @ [ r ] @ rest_r)
 
 let ( <+ ) nel value = match nel with head, rest -> (head, rest @ [ value ])
-let ( +> ) value nel = match nel with head, rest -> (value, [ head ] @ rest)
-let size nel = match nel with _, rest -> List.length rest + 1
-let to_list nel = match nel with head, rest -> head :: rest
-let to_option nel = match nel with head, rest -> Some (head, rest)
-let to_result nel = match nel with head, rest -> Ok (head, rest)
+let ( +> ) value = function head, rest -> (value, [ head ] @ rest)
+let size = function _, rest -> List.length rest + 1
+let to_list = function head, rest -> head :: rest
+let to_option = function head, rest -> Some (head, rest)
+let to_result = function head, rest -> Ok (head, rest)
